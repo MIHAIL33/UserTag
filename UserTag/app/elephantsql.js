ecb789cafa453d76f8c07229b1a15622548f776a
MIHAIL33 <mihail_was96@mail.ru>
2021-11-07 15:38:31 +0300

    Add timeout to connect

--- UserTag/app/elephantsql.js
+++ UserTag/app/elephantsql.js
@@ -1,16 +1,25 @@
 const pg = require('pg')
-const client = new pg.Client(process.env.DATABASE_URL)
-
-client.connect(function(err) {
-    if (err) {
-        return console.error('could not connect to postgres', err);
-    }
-    client.query('SELECT NOW() AS "theTime"', function(err, result) {
-        if (err) {
-            return console.error('error running query', err)
-        }
-        console.log(result.rows[0].theTime)
-    })
+let client = new pg.Client({
+    connectionString: process.env.DATABASE_URL,
 })
+
+function start() {
+    client.connect().then(() => {
+        client.query('SELECT NOW() AS "theTime"', function(err, result) {
+            if (err) {
+                return console.error('error running query', err)
+            }
+            console.log(result.rows[0].theTime)
+        })
+    }).catch(async (err) => {
+        client = new pg.Client({
+            connectionString: process.env.DATABASE_URL,
+        })
+        console.error('could not connect to postgres', err);
+        setTimeout(start, 10000)
+    })
+}
+
+start()
 
 module.exports = client
